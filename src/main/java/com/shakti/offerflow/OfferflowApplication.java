76b2c505c642dd272d6b08a2320fc05427bc986b
Shakti Prasad Sahoo <shaktiprasad278@gmail.com>
2026-07-14 22:27:26 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/shakti/offerflow/OfferflowApplication.java
@@ -0,0 +1,14 @@
+package com.shakti.offerflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferflowApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(OfferflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shakti/offerflow/OfferflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakti.offerflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
